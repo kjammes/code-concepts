b8fb5758b037b27741a52fd4c948dd4ddd61423b
kjammes <jayeshkarli@gmail.com>
2024-03-26 23:54:45 +0530

    code refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        var parallelFib = new ParallelFib();
-        var nums = new int[] {45, 12, 33};
-//        parallelFib.printFibInParallel(nums);
-//        parallelFib.printFibUsingExecutor(nums);
-        parallelFib.printFibInVirtualThread(nums);
-//        parallelFib.printFibInCompletableFutures(nums);
-//        parallelFib.printFibInVirtualThreadRaw(nums);
+        callParallelFibFinders();
+    }
+
+    private static void callParallelFibFinders() {
+        final var parallelFib = new ParallelFib();
+        final var nums = new int[] {45, 12, 33};
+
+        try {
+            parallelFib.printFibInParallel(nums);
+            parallelFib.printFibUsingExecutor(nums);
+            parallelFib.printFibInVirtualThread(nums);
+            parallelFib.printFibInCompletableFutures(nums);
+            parallelFib.printFibInVirtualThreadRaw(nums);
+        } catch (Exception e) {
+            System.out.println("Exception in finding fib in parallel " + e.getLocalizedMessage());
+        }
     }
 }
